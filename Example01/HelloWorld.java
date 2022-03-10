2af5d853021c940b4de7d14e6e42b837bf9e9a82
Yeison <enmanuelquiterios@gmail.com>
2022-03-10 14:08:51 -0500

    initalized Java files

---
+++ Example01/HelloWorld.java
@@ -0,0 +1,10 @@
+package Example01;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        String nameUser = "Yeison ";
+        String lastNameUser = "Casado";
+        System.out.println(nameUser+ lastNameUser);
+    }
+    
+}
