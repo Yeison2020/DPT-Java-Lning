825acc21abe0c5ac192228fcefdf5d4d5e48b9b6
Yeison <enmanuelquiterios@gmail.com>
2022-03-10 15:09:01 -0500

    Working with functions

--- Example01/Problem.java
+++ Example01/Problem.java
@@ -1,5 +1,8 @@
 package Example01;
-import java.io.BufferedInputStream;
+
+
+
+// Imports from Java
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -29,7 +32,11 @@ public class Problem {
         System.out.println("Add Your third Number here");
         String strNumber3 = buffer.readLine();
         Integer number3 = Integer.parseInt(strNumber3);
+
+
+        // Functions to handle and System.out.println my inputs
         calculateHigherNumber(number1,number2,number3);
+        calculateLowerNumber(number1,number2,number3);
 
 
     }
@@ -43,6 +50,17 @@ public class Problem {
             Numbermayor = number3;
         }
         System.out.println("The biggest Number here is = " + Numbermayor);
+    }
+
+    
+    private static void calculateLowerNumber(Integer number1,Integer number2,Integer number3){
+        Integer Numbermayor = number1;
+        if (number2 < Numbermayor){
+            Numbermayor = number2;
+        } if (number3 < Numbermayor){
+            Numbermayor = number3;
+        }
+        System.out.println("The Lowest Number here is = " + Numbermayor);
     }
     
 }
